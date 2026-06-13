add4838ed63a8e1e4ee519c2c446b96c6e2ea2f8
Krishna{"Rajput"} <krishna.rajput21official@gmail.com>
2026-06-13 17:40:53 +0530

    Time: 99 ms (5.04%) | Memory: 274.3 MB (5.01%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string d=s;
+        d.push_back(' ');
+        string ans;
+        string sub;
+        int j=0;
+        for(int i=0; i<d.size(); i++){
+            if(d[i]==' '){
+                sub=d.substr(j,i-j);
+                reverse(sub.begin(),sub.end());
+                ans=ans+sub+' ';
+                j=i+1;
+            }
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
