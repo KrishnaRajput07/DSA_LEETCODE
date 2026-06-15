b0227e696369a86723ee3b68b7323eeee7f382cc
Krishna{"Rajput"} <krishna.rajput21official@gmail.com>
2026-06-15 11:45:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (96.58%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    void helper(int n,int c,vector<string> &ans,string level){
+    void helper(int n,int c,vector<string> &ans,string &level){
         if(n==0){
+            int temp = c;
           if(c!=0){
            while(c!=0){
            level.push_back(')');
@@ -9,6 +10,9 @@ public:
             }
           }
           ans.push_back(level);
+          while (temp--) {
+                level.pop_back();
+            }
           return;
         }
         if(level.empty()){
@@ -22,6 +26,7 @@ public:
            if(c>n){
             level.push_back(')');
            helper(n,c-1,ans,level);
+           level.pop_back();
            }
         }
         return;
