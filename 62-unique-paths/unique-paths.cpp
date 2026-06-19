3f7ad69f578da1249b075662a0d8e438722587d3
Krishna{"Rajput"} <krishna.rajput21official@gmail.com>
2026-06-19 16:54:10 +0530

    Time: 3 ms (9.64%) | Memory: 9.5 MB (28.99%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int helper(vector<vector<int>> &dp,int i,int j ){
+        if(i==0 && j==0) return dp[i][j]=1;
+        else if(i==0||j==0) return dp[i][j]=1;
+        if(dp[i][j] != -1) return dp[i][j];
+        else{
+           dp[i][j]=helper(dp,i-1,j)+helper(dp,i,j-1);
+        }
+        return dp[i][j];
+    }
+    int uniquePaths(int m, int n) {
+       vector<vector<int>> dp(m, vector<int>(n,-1));
+       return helper(dp,m-1,n-1); 
+    }
+};
