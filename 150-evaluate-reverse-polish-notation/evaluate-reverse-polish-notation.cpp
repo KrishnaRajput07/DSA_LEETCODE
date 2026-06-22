2c87ecd0dbdf31e18530f2920eed61fefbecf4e9
Krishna{"Rajput"} <krishna.rajput21official@gmail.com>
2026-06-22 21:14:58 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (40.52%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(int i=0; i<tokens.size(); i++){
+            if(tokens[i]=="+" || tokens[i]=="-"|| tokens[i]=="*"|| tokens[i]=="/" ){
+                int a=st.top();
+                st.pop();
+                int b=st.top();
+                st.pop();
+                int c;
+                char ch=tokens[i][0];
+                switch(ch){
+                    case '+':
+                     c=b+a;
+                     break;
+                    case '-':
+                     c=b-a;
+                     break;
+                    case '*':
+                     c=b*a;
+                     break;
+                    case '/':
+                     c=b/a;
+                     break;
+                }
+                st.push(c);
+            }
+            else{
+                st.push(stoi(tokens[i]));
+            }
+        }
+        return st.top();
+    }
+};
